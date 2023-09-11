4b7796f7f25f3ad492328f0f68c4344e672833e9
Sandesh0202 <sandeshpargaonkar7@gmail.com>
2023-09-11 22:29:42 +0530

    Updated App, is Working fine

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pickle 
+import numpy as np
+import pandas as pd
+from utils import mapping_dict, smoker_map, columns
 
 model = pickle.load(open('predictor.pkl', 'rb'))
 
@@ -11,3 +14,15 @@ children = st.slider("Choose Children",0,5)
 smoker = st.select_slider("Choose Smoker", ['Smokes', 'Not Smokes'])
 region = st.selectbox("Choose Region", ['southwest', 'southeast', 'northwest', 'northeast'])
 
+def predict():
+    row  = np.array([age, bmi, children, smoker_map[smoker], mapping_dict[region]])
+    X = pd.DataFrame([row], columns=columns)
+    prediction = model.predict(X)
+    return prediction[0]
+    
+
+trigger = st.button("Predict", on_click=predict)
+
+if trigger:
+    prediction = predict()
+    st.write(f"Predicted Insurance Charges: {prediction:.2f}")
---
+++ utils.py
@@ -0,0 +1,3 @@
+mapping_dict = {'southwest': 0, 'southeast': 1, 'northwest': 2, 'northeast': 3}
+smoker_map = {'Smokes': 1, 'Not Smokes': 0}
+columns = ['age', 'bmi', 'children', 'smoker', 'region']
